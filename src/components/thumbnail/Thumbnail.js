04d8f8b8896c97f5d4f4029338a6f5e0da103b8d
RuthMoyano <77814331+RuthMoyano@users.noreply.github.com>
2021-06-28 01:40:31 -0500

    Create Thumbnail.js

---
+++ src/components/thumbnail/Thumbnail.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+export class Thumbnails extends Component {
+  render() {
+    return (
+      <div style={{ display: "flex", flexWrap: "wrap" }}>
+        <div>
+          {this.props.images.map((item, index) => {
+            return (
+              <img
+                style={{ height: 150, width: 150 }}
+                key={item}
+                src={item}
+                alt={item}
+                onClick={() => this.props.thumbnailsSelect(item, index)}
+              />
+            );
+          })}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Thumbnails;
