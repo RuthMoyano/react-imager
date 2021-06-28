f42878199840f1906f45da6443764478f03fe280
RuthMoyano <77814331+RuthMoyano@users.noreply.github.com>
2021-06-28 01:11:19 -0500

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+import Imager from "./components/imager/Imager";
+
+function App() {
+  return (
+    <div className="App">
+      <Imager />
+    </div>
+  );
+}
+export default App;
